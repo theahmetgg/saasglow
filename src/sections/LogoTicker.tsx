ec26b5acfdbc0109a8043299d349d38218197b8f
theahmetgg <ahmet1998gulmez@gmail.com>
2024-08-07 23:14:24 +0300

    Add responsive design to Logo Ticker section

--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -1,7 +1,48 @@
+import acmeLogo from "@/assets/logo-acme.png";
+import quantumLogo from "@/assets/logo-quantum.png";
+import echoLogo from "@/assets/logo-echo.png";
+import celestialLogo from "@/assets/logo-celestial.png";
+import pulse from "@/assets/logo-pulse.png";
+import apexLogo from "@/assets/logo-apex.png";
+import Image from "next/image";
+
 const LogoTicker = () => {
   return (
-    <div>LogoTicker</div>
-  )
-}
+    <div className="py-8 bg-white md:py-12">
+      <div className="container">
+        <div className="flex overflow-hidden [mask-image:linear-gradient(to_right,transparent,black,transparent)]">
+          <div className="flex gap-14 flex-none">
+            <Image
+              src={acmeLogo}
+              alt="Acme Logo"
+              className="logo-ticker-image"
+            />
+            <Image
+              src={quantumLogo}
+              alt="quantum Logo"
+              className="logo-ticker-image"
+            />
+            <Image
+              src={echoLogo}
+              alt="echoLogo"
+              className="logo-ticker-image"
+            />
+            <Image
+              src={celestialLogo}
+              alt="celestial Logo"
+              className="logo-ticker-image"
+            />
+            <Image src={pulse} alt="pulse Logo" className="logo-ticker-image" />
+            <Image
+              src={apexLogo}
+              alt="apex Logo"
+              className="logo-ticker-image"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default LogoTicker
+export default LogoTicker;
