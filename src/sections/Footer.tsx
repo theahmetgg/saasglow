3db5e6269a69fdfaf9e0f96e977c02e4860c7c88
theahmetgg <ahmet1998gulmez@gmail.com>
2024-08-09 17:11:30 +0300

    Add Footer section

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,7 +1,40 @@
+import logo from "@/assets/logosaas.png";
+import SocialX from "@/assets/social-x.svg";
+import SocialInsta from "@/assets/social-insta.svg";
+import SocialLinkedin from "@/assets/social-linkedin.svg";
+import SocialPin from "@/assets/social-pin.svg";
+import SocialYoutube from "@/assets/social-youtube.svg";
+import Image from "next/image";
+
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="bg-black text-[#BCBCBC] text-sm py-10 text-center">
+      <div className="container">
+        <div className="inline-flex relative before:content-[''] before:h-full before:w-full before:top-2 before:bottom-0 before:blur before:bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500  before:absolute">
+          <Image src={logo} height={40} alt="SaaS logo" className="relative" />
+        </div>
+        <nav className="flex flex-col md:flex-row md:justify-center gap-6 mt-6">
+          <a href="#">About</a>
+          <a href="#">Features</a>
+          <a href="#">Customers</a>
+          <a href="#">Pricing</a>
+          <a href="#">Help</a>
+          <a href="#">Careers</a>
+        </nav>
 
-export default Footer
+        <div className="flex justify-center gap-6 mt-6">
+          <SocialX />
+          <SocialInsta />
+          <SocialLinkedin />
+          <SocialPin />
+          <SocialYoutube />
+        </div>
+        <p className="mt-6">
+          &copy; 2024 Your Company,Inc. All rights reserved
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
